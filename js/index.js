e9830729e52b531dda7fb0a8e777646934769252
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-10-04 20:15:37 +0300

    Summary

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
-document.querySelector('#btnClick').addEventListener('click', changeLogo);
-
-const maxWordLength = 6;
+btnClick.addEventListener('click', changeLogo);
 
 const searchInput = document.querySelector('.main_search');
-function changeLogo(e) {
-  const googleLogo = document.querySelector('#googleLogo').childElementCount;
-  googleLogo > maxWordLength
-    ? (searchInput.firstElementChild.innerHTML = `
-      <span id="G">G</span>
-      <span id="o">o</span>
-      <span id="oo">o</span>
-      <span id="G">g</span>
-      <span id="l">l</span>
-      <span id="o">e</span>
-   `)
-    : (searchInput.firstElementChild.innerHTML = `
-      <span>I</span>
-      <span>L</span>
-      <span>M</span>
-      <span>A</span>
-      <span>K</span>
-      <span>I</span>
-      <span>A</span>
-      <span>G</span>
-      <span>E</span>
-      `);
+function changeLogo() {
+  if (btnClick.hasAttribute('data-change')) {
+    searchInput.firstElementChild.innerHTML = `
+    <span>I</span>
+    <span>L</span>
+    <span>M</span>
+    <span>A</span>
+    <span>K</span>
+    <span>I</span>
+    <span>A</span>
+    <span>G</span>
+    <span>E</span>
+    `;
+    btnClick.removeAttribute('data-change');
+  } else {
+    searchInput.firstElementChild.innerHTML = `
+  <span id="G">G</span>
+  <span id="o">o</span>
+  <span id="oo">o</span>
+  <span id="G">g</span>
+  <span id="l">l</span>
+  <span id="o">e</span>
+`;
+    btnClick.setAttribute('data-change', 'google');
+  }
 }
